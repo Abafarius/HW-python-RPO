8f15401d88f401f05fa2821407301d85f5227cc0
gal.esm49 <esm.2001@mail.ru>
2023-11-08 00:21:44 +0600

    add files imgs and 2 py files

---
+++ 06.11.2023/week1.py
@@ -0,0 +1,2 @@
+print("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", sep=", ")
+#This program displays days' name of week with comma separator 
---
+++ 06.11.2023/week2.py
@@ -0,0 +1,2 @@
+print("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", sep=", ", end=".")
+#This program prints days of week with separator comma and ends with dot
