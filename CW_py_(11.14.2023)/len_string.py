11d91ff263d3c27dee4b351e6a387dc10fc70958
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-11-14 21:50:33 +0600

    create classwork files 11.14

---
+++ CW_py_(11.14.2023)/len_string.py
@@ -0,0 +1,5 @@
+user_data = input("Введите строку:")
+
+count_user_data = len(user_data)
+
+print("Ваша строка длиной:", count_user_data, "символов")
---
+++ CW_py_(11.14.2023)/unicode_abc+1.py
@@ -0,0 +1,5 @@
+ud = input("Enter a letter:")
+unicode_num = ord(ud)
+plus_one = unicode_num + 1
+character = chr(plus_one)
+print(character)
