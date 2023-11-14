26d3c00e7cde488fd52da31af2debf30d87db107
gal.esm49 <esm.2001@mail.ru>
2023-11-15 02:27:36 +0600

    Update simple_round.py

--- 11.14.2023/simple_round.py
+++ 11.14.2023/simple_round.py
@@ -1,4 +1,4 @@
 float_num = float(input("Enter float number:"))
 int_num_count = int(input("Enter float number count:"))
 round_fun = round(float_num, int_num_count)
-print(round_fun)
+print("Here is your rounded number:", round_fun)
