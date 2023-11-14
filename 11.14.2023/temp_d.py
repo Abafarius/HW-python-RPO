69c2b17994856c6734bd0690ad25bfda802e723f
gal.esm49 <esm.2001@mail.ru>
2023-11-15 02:31:25 +0600

    Update temp_d.py

--- 11.14.2023/temp_d.py
+++ 11.14.2023/temp_d.py
@@ -1,4 +1,4 @@
-day_temp = int(input("Ener day temp:"))
+day_temp = int(input("Enter day temp:"))
 night_temp = int(input("Enter night temp:"))
 disperse_temp = abs(day_temp-night_temp)
-print(disperse_temp)
+print("Here is difference between day and night temperature:", disperse_temp)
