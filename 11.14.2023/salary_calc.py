de1c8901d7e839b45cb90209dd18d9722e117869
gal.esm49 <esm.2001@mail.ru>
2023-11-15 03:29:29 +0600

    Update salary_calc.py

---
+++ 11.14.2023/salary_calc.py
@@ -0,0 +1,5 @@
+user_size_sales = input("How much did you sell: ")
+size_sales = int(user_size_sales)
+salary_const = 250
+salary_summ = salary_const+(size_sales*0.1)
+print("Your salary size is: ", salary_summ)
