616b3ab27d4c6c32e60e22a4e420cc8c6ffa4bbc
gal.esm49 <esm.2001@mail.ru>
2023-11-15 22:39:43 +0600

    Update digit_converter.py

--- 11.14.2023/digit_converter.py
+++ 11.14.2023/digit_converter.py
@@ -1,6 +1,17 @@
+#Asking a number from user
 user_dec = input("Enter decimal number:")
+
+#Translating user data to integer
 str_to_int = int(user_dec)
+
+#Translating from decimal to binary 
 binary_num = bin(str_to_int)
+
+#Translating from decimal to hex 
 hex_num = hex(str_to_int)
+
+#Translating from decimal to oct 
 oct_num = oct(str_to_int)
+
+#print result
 print("Binary num: ", binary_num, "\nHex num: ", hex_num, "\nOct num:", oct_num)
