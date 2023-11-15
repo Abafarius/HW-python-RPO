69cc00a1ccbd847ab43e2f13c71e7e601fc0984a
gal.esm49 <esm.2001@mail.ru>
2023-11-15 23:01:44 +0600

    Update simple_round.py

--- 11.14.2023/simple_round.py
+++ 11.14.2023/simple_round.py
@@ -1,4 +1,17 @@
-float_num = float(input("Enter float number:"))
-int_num_count = int(input("Enter float number count:"))
+#Asking for float number from user
+float_num_user = input("Enter float number:")
+
+#Asking for count of numbers after comma from user
+int_num_count_user = int(input("Enter count of numbers after comma (must be integer):"))
+
+#Translating to float 
+float_num = float(float_num_user)
+
+#Translating to int
+int_num_count = int(int_num_count_user)
+
+#Round function
 round_fun = round(float_num, int_num_count)
+
+#print result
 print("Here is your rounded number:", round_fun)
