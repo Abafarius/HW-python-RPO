5167dccfabc75a1f536709559519c5067f9caa9d
gal.esm49 <esm.2001@mail.ru>
2023-11-15 22:41:23 +0600

    Update distance_dots.py

--- 11.14.2023/distance_dots.py
+++ 11.14.2023/distance_dots.py
@@ -1,13 +1,17 @@
+#Asking for coordinates from user
 user_x1 = input("Enter 1st x dot: ")
 user_x2 = input("Enter 2st x dot: ")
 user_y1 = input("Enter 1nd y dot: ")
 user_y2 = input("Enter 2nd y dot: ")
 
+#Translating user data to integer
 x1 = int(user_x1)
 x2 = int(user_x2)
 y1 = int(user_y1)
 y2 = int(user_y2)
 
+#Calculating distance between 2 dots
 distance_dots = ((x1-x2)**2+(y1-y2)**2)**0.5
 
+#print result
 print("distance between dots: ", distance_dots)
