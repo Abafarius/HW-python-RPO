990b7d2d4ccf052bdea4645b236f433c6e5d6a46
gal.esm49 <esm.2001@mail.ru>
2023-11-15 22:36:47 +0600

    Update clock_calc.py

--- 11.14.2023/clock_calc.py
+++ 11.14.2023/clock_calc.py
@@ -1,13 +1,18 @@
+#Asking for numbers
 user_hour_num = input("Enter a current hour time:")
 user_minute_num = input("Enter a current minute time:")
 
+#declaring constants 
 hour_const = 23
 minute_const = 60
 
+#Translating user data to integer
 hour = int(user_hour_num)
 minute = int(user_minute_num)
 
+#Calculating hours and minutes until end of a day
 calculation_hour = abs(hour_const-hour)
 calculation_minute = abs(minute_const-minute)
 
+#print result
 print("There's still", calculation_hour,"hours and", calculation_minute,"minute untill tomorrow")
