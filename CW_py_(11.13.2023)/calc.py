d65a39f0fb54ea65802b8b748e1ec9230b253a4f
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-11-18 20:22:47 +0600

    Проверено дз от 13.11
    Комментарий к коду

--- CW_py_(11.13.2023)/calc.py
+++ CW_py_(11.13.2023)/calc.py
@@ -1,4 +1,7 @@
 user_num1 = int(input("Enter num:"))
 user_num2 = int(input("Enter num 2:"))
 
-print("Result of adding:", user_num1+user_num2, "\nResult of substraction:", user_num1-user_num2, "\nResult of dividing: ", user_num1/user_num2)
+print("Result of adding:", user_num1+user_num2, 
+      "\nResult of substraction:", user_num1-user_num2, 
+      "\nResult of dividing: ", user_num1/user_num2)
+#VN: длинные выражения лучше писать в несколько строк или разбивать на несколько отдельных инструкций
