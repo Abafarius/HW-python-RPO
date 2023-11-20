94fcd5cb61fe17a6daab8a1c957949982e8cc9a1
gal.esm49 <esm.2001@mail.ru>
2023-11-20 22:46:06 +0600

    Update 1. temp_d try.py

--- 11.20.2023/1. temp_d try.py
+++ 11.20.2023/1. temp_d try.py
@@ -1,9 +1,18 @@
 #Asking for day and night temp-s from user
-day_temp = int(input("Enter day temp:"))
-night_temp = int(input("Enter night temp:"))
+day_temp_user = input("Enter day temp:")
+night_temp_user = input("Enter night temp:")
 
-#Calculating temperature difference between day and night
-disperse_temp = abs(day_temp-night_temp)
 
-#print result
-print("Here is difference between day and night temperature:", disperse_temp)
+try:
+    #Translating string to int
+    day_temp = int(day_temp_user)
+    night_temp = int(night_temp_user)
+except ValueError: 
+    print("Enter valid number")
+else:
+    #Calculating temperature difference between day and night
+    disperse_temp = abs(day_temp-night_temp)
+    #print result
+    print("Here is difference between day and night temperature:", disperse_temp)
+
+
