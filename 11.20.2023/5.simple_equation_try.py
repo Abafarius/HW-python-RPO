40053d1a6291d5159d3a7e85f684c9df00646c9a
gal.esm49 <esm.2001@mail.ru>
2023-11-21 16:54:22 +0600

    Update 5.simple_equation_try.py

--- 11.20.2023/5.simple_equation_try.py
+++ 11.20.2023/5.simple_equation_try.py
@@ -2,12 +2,30 @@
 user_a_num = input("Enter <a> number:")
 user_b_num = input("Enter <b> number:")
 
-#Translating user data to integer
-a = int(user_a_num)
-b = int(user_b_num)
+def_num_a = 137
+def_num_b = 1
+def_num_x = -0.0072992700729927005
 
-#Calculating x
-x = -b/a
+#Translating user data to integer
+try:
+    a = int(user_a_num)
+    
+except ValueError:
+    print("\nEnter valid number. Now default a value is active:", def_num_a)
+    a = def_num_a
+
+try: 
+    b = int(user_b_num)
+except ValueError:
+    print("Enter valid number. Now default b value is active:", def_num_b, "\n")
+    b = def_num_b
+
+try:
+    #Calculating x
+    x = -b/a
+except ZeroDivisionError:
+    print("Number a can't be zero, we can't divide to zero. Anyway default x num is:", x)
+    x = def_num_x
 
 #print result
 print("Here is you x number:", x)
