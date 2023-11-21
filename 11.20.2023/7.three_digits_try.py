bf7afdf8578aeacf9c6c45e7b552b7a4a1f37926
gal.esm49 <esm.2001@mail.ru>
2023-11-21 17:16:19 +0600

    Update 7.three_digits_try.py

--- 11.20.2023/7.three_digits_try.py
+++ 11.20.2023/7.three_digits_try.py
@@ -1,9 +1,14 @@
 # Asking for three digited number
-number_user = input("Введите трехзначное число: ")
+number_user = input("Enter a three-digit number: ")
+
+def_num = 137
 
 #Translating from string to integer
-number = int(number_user)
-
+try:
+    number = int(number_user)
+except ValueError:
+    print("Wrong value. Default number 137 is active")
+    number = def_num
 # calculating second digit of number
 second_digit = (number // 10) % 10
 
