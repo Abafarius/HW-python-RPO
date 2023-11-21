fe2aa60f4946f4b6deddf7b12a20afd469945b87
gal.esm49 <esm.2001@mail.ru>
2023-11-21 17:07:41 +0600

    Update 6.clock_calc_try.py

--- 11.20.2023/6.clock_calc_try.py
+++ 11.20.2023/6.clock_calc_try.py
@@ -7,8 +7,17 @@ hour_const = 23
 minute_const = 60
 
 #Translating user data to integer
-hour = int(user_hour_num)
-minute = int(user_minute_num)
+try: 
+    hour = int(user_hour_num)
+except: 
+    print("\nYou entered wrong value for hour. Please, enter integer number from 0 to 23. Now default value is active:", hour_const)
+    hour = hour_const
+
+try:
+    minute = int(user_minute_num)
+except:
+    print("You entered wrong value for minute. Please, enter integer number from 0 to 60. Now default value is active:", minute_const, "\n")
+    minute = minute_const
 
 #Calculating hours and minutes until end of a day
 calculation_hour = abs(hour_const-hour)
