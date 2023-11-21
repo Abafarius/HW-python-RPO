72ffa3836164bab4019be92466d14110c306b6a7
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-11-21 20:03:13 +0600

    Update 00.py

---
+++ CW_py_(11.21.2023)/00.py
@@ -0,0 +1,20 @@
+cost = 1500
+if cost < 1000:
+    print("NO DISCOUNT")
+elif cost < 2000:
+   print("discount 2%")
+elif cost <5000:
+    print("discount 5%")
+else:
+    print("discont 20%")
+
+print("delif -> if")
+
+if cost < 1000:
+    print("NO DISCOUNT")
+if cost < 2000:
+    print("discount 2%")
+if cost <5000:
+    print("discount 5%")
+else:
+    print("discont 10%")
