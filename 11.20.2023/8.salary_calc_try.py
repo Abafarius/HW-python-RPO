c878106008786de501ebcc9bfc40b7a37ed07ad7
gal.esm49 <esm.2001@mail.ru>
2023-11-21 17:19:17 +0600

    Update 8.salary_calc_try.py

--- 11.20.2023/8.salary_calc_try.py
+++ 11.20.2023/8.salary_calc_try.py
@@ -1,14 +1,14 @@
 #Asking size of sales from user 
 user_size_sales = input("How much did you sell: ")
-
-#Translating user data to integer
-size_sales = int(user_size_sales)
-
-#Declaring constant
+default_sales = 100
 salary_const = 250
+#Translating user data to integer
+try: 
+    size_sales = int(user_size_sales)
+except ValueError:
+    print("Error when transformation. Value of sales choosed to default number")
+    size_sales = default_sales
 
-#Calculating average salary
+
 salary_summ = salary_const+(size_sales*0.1)
-
-#print result
-print("Your salary size is: ", salary_summ)
+print("Your salary size is: ", salary_summ, "$", sep="")
