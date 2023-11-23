28f4f1b6e45dc5c66e3e7145ccb12ab0b7f24844
gal.esm49 <esm.2001@mail.ru>
2023-11-23 19:36:16 +0600

    Update 3.leapYear.py

---
+++ 11.21.2023/3.leapYear.py
@@ -0,0 +1,20 @@
+ud = input("Enter year:")
+
+try:
+    year = int(ud)
+except ValueError:
+    print("Value error just occured! You must type integer number!")
+else:
+    if 0<=year<=2100:
+        if year % 4 == 0:
+            if year % 100 ==0:
+                if year % 400 == 0:
+                    print("leap year")
+                else:
+                    print("not leap year")
+            else:
+                print("leap year")
+        else: 
+            print("It's not a leap year")
+    else:
+        print("I don't want to calculate the far future or the far past")
