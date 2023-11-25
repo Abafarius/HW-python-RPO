ce7343893b5e5ce177afe51aa1097abcc85303b4
gal.esm49 <esm.2001@mail.ru>
2023-11-25 23:30:45 +0600

    Update 6.dicount.py

---
+++ 11.21.2023/6.dicount.py
@@ -0,0 +1,20 @@
+userBuy = input("Enter summ how much you bought:")
+
+try:
+    buy = float(userBuy)
+except ValueError:
+    print("Please enter valid number!")
+else:
+    if 0<buy<200:
+        print("No discount. Summ to pay = ", buy)
+    elif 200<=buy<=300:
+        payment = buy*1.03
+        print("You got discount 3%. Your payment is:", payment)
+    elif 300<=buy<=500:
+        payment = buy*1.05
+        print("You got discount 5%. Your payment is:", payment)
+    elif buy>500:
+        payment = buy*1.07
+        print("You got discount 7%. Your payment is:", payment)
+    else:
+        print("Error")
