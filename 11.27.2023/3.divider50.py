7a0d9b37873cf1c6a6d249c8beea8fa4d7090cf0
gal.esm49 <esm.2001@mail.ru>
2023-11-28 10:13:05 +0600

    Update 3.divider50.py

---
+++ 11.27.2023/3.divider50.py
@@ -0,0 +1,16 @@
+num1 = 0
+counter = 0
+user_input1 = input("Enter number:")
+
+
+try:
+    num1 = float(user_input1)
+except ValueError:
+    print("Value Error has just occured. Enter an integer number")
+
+
+while num1 > 50:
+    num1 /= 2
+    counter += 1
+print("Result of dividing: ", num1, "\nTimes you divided: ", counter)
+
