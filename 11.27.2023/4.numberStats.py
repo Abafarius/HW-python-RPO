2c36d13996bfb8443ba7247a70153c28ae1876af
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-11-28 11:57:58 +0600

    Create 4.numberStats.py

---
+++ 11.27.2023/4.numberStats.py
@@ -0,0 +1,29 @@
+user_num = 0
+user_input = ""
+count=0
+neg=0
+pos=0
+zero=0
+even=0
+odd=0
+while count < 10:
+  user_input = input("Enter a number: ")
+  count += 1
+  try:
+    user_num = int(user_input)
+  except: 
+    print("Value Error.")
+  
+  if user_num < 0: 
+    neg+=1
+  if user_num > 1:
+    pos+=1
+  if user_num == 0:
+    zero += 1
+  if user_num % 2 == 0:
+    even += 1
+  else:
+    odd += 1
+print("Negatives: ",neg,"\nPositives: ",pos,"\nzeroes: ",zero, "\nevens: ",even,"\nOdds:",odd)
+    
+    
