813f247c040bae19ed10d92e5f8d6b3f79aa40ef
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-11-28 19:33:08 +0600

    Create 00.py

---
+++ CW_py_(11.28.2023)/00.py
@@ -0,0 +1,9 @@
+for kolbasa in "Python":
+    
+    if kolbasa == "o" or kolbasa == "y":
+        continue
+    if kolbasa == "n":
+        break
+    print(kolbasa)
+else:
+    ("Kolbasa zakonchilas")
