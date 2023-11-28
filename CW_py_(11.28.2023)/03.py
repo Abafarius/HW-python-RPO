bcaf758ad743da5cb230330796c55c62c3adea6e
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-11-28 21:20:36 +0600

    Update 03.py

--- CW_py_(11.28.2023)/03.py
+++ CW_py_(11.28.2023)/03.py
@@ -1,4 +1,9 @@
 user_data = input("Enter a word: ")
-user_length = len(user_data)
-for word_num in range(2,user_length,3):
-    print(user_data[word_num])
+#user_length = len(user_data)
+# for word_num in range(2,user_length,3):
+#     print(user_data[word_num])
+position = 1
+for char in user_data:
+    if position%3 == 0:
+        print(char)
+    position+=1
