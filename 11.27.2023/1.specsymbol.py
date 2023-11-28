ae0a5df10ea6b1e8cdff9e987780c0f8ca89a8b7
gal.esm49 <esm.2001@mail.ru>
2023-11-28 09:08:35 +0600

    Update 1.specsymbol.py

--- 11.27.2023/1.specsymbol.py
+++ 11.27.2023/1.specsymbol.py
@@ -5,25 +5,27 @@ try:
 except ValueError:
     print("Value error just occured! You must type single integer number as: 0,1,2,3,4,5,6,7,8,9.")
 else:
-    if numU == 0:
-        print(")")
-    elif numU == 1:
-        print("!")
-    elif numU == 2:
-        print("@")
-    elif numU == 3:
-        print("#")
-    elif numU == 4:
-        print("$")
-    elif numU == 5:
-        print("%")
-    elif numU == 6:
-        print("^")
-    elif numU == 7:
-        print("&")
-    elif numU == 8:
-        print("*")
-    elif numU == 9:
-        print("(")
-    else:
-        print("no symbol for such number.")
+    match numU:
+        case 0:
+            print(")")
+        case 1:
+            print("!")
+        case 2:
+            print("@")
+        case 3:
+            print("#")
+        case 4:
+            print("$")
+        case 5:
+            print("%")
+        case 6:
+            print("^")
+        case 7:
+            print("&")
+        case 8:
+            print("*")
+        case 9:
+            print("(")            
+        case _:
+            print("no symbol for such number.")
+
