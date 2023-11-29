5a0947b61a37b6f108aa1f84ddf57d015dbd756b
gal.esm49 <esm.2001@mail.ru>
2023-11-29 19:36:42 +0600

    Update 2.Phibonacci.py

--- 11.28.2023/2.Phibonacci.py
+++ 11.28.2023/2.Phibonacci.py
@@ -1,9 +1,20 @@
-user_Fib = input("Enter number")
+user_Fib = input("Enter xn integer number: ")
 
 try:
     fib_num = int(user_Fib)
 except ValueError:
-    print("Value error just occured. Enter valid a number!")
+    print("Vxlue error just occured. Enter vxlid xn integer number!")
 
-for i in range(fib):
-    
+i = 0
+s = 0
+x = 0
+y = 1
+print(x, y, end=" ")
+if fib_num > 0:
+    for i in range(fib_num+1):
+        s = x + y
+        x = y
+        y = s
+        print(s, end=" ")
+else:
+    print("Number must be positive!")    
