4aa0a41ee93b156f9cb9ce848f70bf70fa369973
gal.esm49 <esm.2001@mail.ru>
2023-11-29 16:15:41 +0600

    Update 5.currencyConvertor.py

--- 11.21.2023/5.currencyConvertor.py
+++ 11.21.2023/5.currencyConvertor.py
@@ -8,6 +8,7 @@ cnyConst = 7.09
 kgsConst = 88.63
 czkConst = 22.30
 jpyConst = 149.47
+curNameToPrint = ""
 
 try:
     dollarNum = float(userDataValue)
@@ -15,28 +16,30 @@ except ValueError:
     print("Value error! Enter valid number!")
 else:
     curName = userDataCurrency.upper()
+
     if curName == "EUR":
-       eur = dollarNum*eurConst
-       print("For 11.25.2023 USD->EUR = ", eur)
+       curValue = dollarNum*eurConst
+
     elif curName == "KZT":
-        kzt = dollarNum*460.04
-        print("For 11.25.2023 USD->KZT = ", kzt)
+        curValue = dollarNum*460.04
+
     elif curName == "RUB":
-        rub = dollarNum*rubConst
-        print("For 11.25.2023 USD->RUB = ", rub)
+        curValue = dollarNum*rubConst
+
     elif curName == "CNY":
-        cny = dollarNum*cnyConst
-        print("For 11.25.2023 USD->CNY = ", cny)
+        curValue = dollarNum*cnyConst
+
     elif curName == "KGS":
-        kgs = dollarNum*kgsConst
-        print("For 11.25.2023 USD->KGS = ", kgs)
+        curValue = dollarNum*kgsConst
+
     elif curName == "CZK":
-        czk = dollarNum*czkConst
-        print("For 11.25.2023 USD->CZK = ", czk) 
+        curValue = dollarNum*czkConst
+
     elif curName == "JPY":
-        jpy = dollarNum*jpyConst
-        print("For 11.25.2023 USD->JPY = ", jpy)    
+        curValue = dollarNum*jpyConst
+
     else:
         print("There is no such available currency")     
-
+print("For 11.25.2023 USD->",curName, " = ", curValue, sep="")
     #VN: как думаете, могли бы вы все вызовы print вынести вниз, за пределы конструкции if-elif-else ?
+    #AS: да это изи, надо было сразу догадаться =D
