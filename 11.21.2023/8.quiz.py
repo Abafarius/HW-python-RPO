b107250db17121921d3237132cb70e89ba70b88c
gal.esm49 <esm.2001@mail.ru>
2023-11-29 15:11:57 +0600

    Update 8.quiz.py

--- 11.21.2023/8.quiz.py
+++ 11.21.2023/8.quiz.py
@@ -1,74 +1,38 @@
-print("Question 1. How many planets in our Solar system?\na)8\nb)9\nc)10")
-ans1 = input("\nAnswer is (format answer 'a,b,c'):")
+print("\nQuestion 1. How many planets in our Solar system?\na)8\nb)9\nc)10")
+q1 = input("\nAnswer is (format answer 'a,b,c'):")
 player = 0
 addPoint = 2
-if ans1 == "a":
-    player = addPoint
-    print("correct! You got", player, "points.")
-
-    print("\nQuestion 2. What is the main fuel of stars?\na)He2\nb)H2\nc)C")
-    ans2 = input("\nAnswer is (format answer 'a,b,c'):")
-
-
-    if ans2 == "b":
-        player += addPoint
-        print("\ncorrect! You got", player, "points.")
-
-        print("\nQuestion 3. According to goldilock baby universe theory what was tempereature of universe in range 10-17 million years after the Big Bang?\na)-0K\nb)1000K\nc)100 celsius")
-        ans3 = input("Answer is (format answer 'a,b,c'):")
+ans1 = q1.upper()
+if ans1 == "A":
+    player += addPoint
+    print("\nRight! Next question:")
+else:
+    print('\nWrong! Next question:')
 
 
-        if ans3 == "c":
-            player += addPoint
-            print("\nYou are cool! you got",player,"points in total!!!\n")
-        else: 
-            print("Answer 3 is wrong! Anyway you got",player,"points in total")
+print("\nQuestion 2. What is the main fuel of stars?\na)He2\nb)H2\nc)C")
+ans2 = input("\nAnswer is (format answer 'a,b,c'):")
 
 
-    else:
-        print("Answer 2 is wrong!")
+if ans2 == "b":
+    player += addPoint
+    print("\nRight! Next question:")
+else:
+    print('\nWrong! Next question:')
 
-        print("\nQuestion 3. According to goldilock baby universe theory what was tempereature of universe in range 10-17 million years after the Big Bang?\na)-0K\nb)1000K\nc)100 celsius")
-        ans3 = input("Answer is (format answer 'a,b,c'):")
-
-
-        if ans3 == "c":
-            player += addPoint
-            print("\nYou are cool! you got",player,"points in total!!!\n")
-        else: 
-            print("Answer 3 is wrong! Anyway you got",player,"points in total")
-else: 
-    print("\nanswer 1 is Wrong!\n")
-
-    print("Question 2. What is the main fuel of stars?\na)He2\nb)H2\nc)C")
-    ans2 = input("\nAnswer is (format answer 'a,b,c'):")
+print("\nQuestion 3. According to goldilock baby universe theory what was tempereature of universe in range 10-17 million years after the Big Bang?\na)-0K\nb)+1000K\nc)+100 celsius")
+ans3 = input("Answer is (format answer 'a,b,c'):")
 
 
-    if ans2 == "b":
-        player += addPoint
-        print("\ncorrect! You got", player, "points.")
+if ans3 == "c":
+    player += addPoint
+    print("\nRight!\n")
+else:
+    print('\nWrong!\n')
 
-        print("\nQuestion 3. According to goldilock baby universe theory what was tempereature of universe in range 10-17 million years after the Big Bang?\na)-0K\nb)1000K\nc)100 celsius")
-        ans3 = input("\nAnswer is (format answer 'a,b,c'):")
-
-
-        if ans3 == "c":
-            player += addPoint
-            print("\nYou are cool! you got",player,"points in total!!!")
-        else: 
-            print("\nAnswer 3 is wrong! Anyway you got",player,"points in total")
-
-    else:
-        print("\nAnswer 2 is wrong!")
-        
-        print("\nQuestion 3. According to goldilock baby universe theory what was tempereature of universe in range 10-17 million years after the Big Bang?\na)-0K\nb)1000K\nc)100 celsius")
-        ans3 = input("\nAnswer is (format answer 'a,b,c'):")
+print("you got",player,"points in total")
 
 
-        if ans3 == "c":
-            player += addPoint
-            print("\nYou are cool! you got",player,"points in total!!!\n")
-        else: 
-            print("\nAnswer 3 is wrong! Anyway you got",player,"points in total")
     
 # VN: здесь прям настоятельно рекомендую убрать подсчёт очков и print'ы, с этим связанные, в конец программы!
+# AS: вроде все исправил
