32c02ba3572052554501f1d2dd8ccb07a87657b8
gal.esm49 <esm.2001@mail.ru>
2023-11-29 15:59:25 +0600

    Update 7.circleInSquare.py

--- 11.21.2023/7.circleInSquare.py
+++ 11.21.2023/7.circleInSquare.py
@@ -1,23 +1,24 @@
 import math
-user_rad = input("Enter the radius of the circle: ")
-user_side_length = input("Enter the side of the square: ")
+
+circle_length = input("Enter the length of the circle: ")
+square_perimeter = input("Enter the side of the square: ")
 
 try:
-    radius = float(user_rad)
-    side_length = float(user_side_length)
-
+    radius = float(circle_length)
+    side_length = float(square_perimeter)
 except ValueError:
-
     print("Please enter valid numeric values.")
-else:
-    circle_area = math.pi * radius**2
-    square_area = side_length**2
 
-    if circle_area <= square_area:
-        print("The circle can fit within the specified square.")
+if circle_length > 0 and square_perimeter > 0:
+    radius = circle_length / (2 * math.pi)
+    side_length = square_perimeter / 4
+    if side_length >= 2 * radius:
+        print("Окружность может поместиться в указанном квадрате.")
     else:
         print("The circle can't fit within the specified square.")
 
 #VN: условие того что окружность впишется в квадрат, не имеет никакого отношения к площади.
 # Всего лишь сторона квадрата должна быть больше диаметра окружности.
 # Обращаю ещё внимание, что по условию задачи пользователь вводит периметр квадрата, а не длину стороны!
+
+#AS: в этот раз я нашел радиус окружности по длине, по заданному пользавателем. Затем нашел одну сторону квадрата поделив периметр на 4. И сравнил сторону с диаметром окружности.
