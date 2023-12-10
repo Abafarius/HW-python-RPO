e35bcd11f82b21f73eba972564da7ff6dee4645d
gal.esm49 <esm.2001@mail.ru>
2023-12-11 00:03:51 +0600

    Update 3.remove_same.py

--- 12.05.2023/3.remove_same.py
+++ 12.05.2023/3.remove_same.py
@@ -1,1 +1,8 @@
-list_num = [3, 9, 8, 4, 5, 1]
+list_num1 = [11, 9, 15, 2, 5, 1]
+list_num_temp = []
+list_num2 = [1, 8, 9, 5, 4, 3, 6]
+for i in list_num1:
+    if i not in list_num2:
+        list_num_temp.append(i)
+list_num1 = list_num_temp
+print(list_num1)
