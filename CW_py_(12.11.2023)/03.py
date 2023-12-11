77461f2f1b920ce5bcf8b26af9a332bbc6606400
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-11 21:35:57 +0600

    Update 03.py

---
+++ CW_py_(12.11.2023)/03.py
@@ -0,0 +1,9 @@
+lyrics = """
+i don't wanna close my eyEE
+i don't wanna FALL asleep
+cause i miss you baby
+and i don't want to miss a thing
+"""
+print(lyrics)
+result = lyrics.replace("i ", "u ")
+print(result)
