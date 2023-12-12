e323902439c56536d4a9bf551dc6a8e8be296098
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-12 20:14:21 +0600

    Update 01.py

--- CW_py_(12.12.2023)/01.py
+++ CW_py_(12.12.2023)/01.py
@@ -1,10 +1,10 @@
 filename = 'input.txt'
 try:
-    my_file = open(filename)
+    my_file = open(filename, encoding='utf-8')
 except FileNotFoundError:
     print("File not Found")
     exit()
 
-text = my_file.read
+text = my_file.read()
 my_file.close()
 print(text)
