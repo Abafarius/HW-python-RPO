21a503cd52c82f236e4a21510c83a4b80d8a960b
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-12 21:47:11 +0600

    Update 03.py

--- CW_py_(12.12.2023)/03.py
+++ CW_py_(12.12.2023)/03.py
@@ -2,4 +2,25 @@
 #программа -> код
 #ПРОГРАММА -> КОД
 #Программа -> Код
+filename = 'article.txt'
+try:
+    file = open(filename, encoding='utf-8')
+except FileNotFoundError:
+    print("File not Found")
+    exit()
+
+text = file.read()
+file.close()
+print(text)
+user_input_old = input("Enter a word to replace: ")
+user_input_new = input("Enter a new word to replace: ")
+user_old_lower = user_input_old.lower()
+user_new_lower = user_input_new.lower()
+result0 = text.replace(user_old_lower, user_new_lower)
+user_input_old.capitalize()
+user_input_old.upper()
+
+result = text.replace(user_input_old, user_input_new)
+print(result)
+
 
