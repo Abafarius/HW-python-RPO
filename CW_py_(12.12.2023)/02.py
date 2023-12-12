86cb2d2be3caccc18ef3a295218816c780827769
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-12 21:10:27 +0600

    Update 02.py

--- CW_py_(12.12.2023)/02.py
+++ CW_py_(12.12.2023)/02.py
@@ -22,7 +22,7 @@ for user_email in email_list:
     email_parts = user_email.split("@")
     parts_ok = len(email_parts) == 2
     first_part = bool(email_parts[0])
-    point_exists = not "." in email_parts[1]
+    point_exists = "." in email_parts[1]
     domain = email_parts[1].split(".")
     domain_ok = len(domain[-1]) > 1
 
