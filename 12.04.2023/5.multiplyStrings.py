c185cd34116d20e5a20277d685137cb86259e131
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-14 13:44:26 +0600

    Update 5.multiplyStrings.py

--- 12.04.2023/5.multiplyStrings.py
+++ 12.04.2023/5.multiplyStrings.py
@@ -1,14 +1,29 @@
 length_user = input("Enter length  of string: ")
 
+
 try:
     length = int(length_user)
 except ValueError:
     print("Error!")
     exit()
 
-givenChars = "C1 C2 " * length
-print(givenChars)
+user_symbol1 = input("Enter a symbol: ")
+user_symbol2 = input("Enter a symbol: ")
+
+result_string = ""
+
+for i in range(length):
+    if i % 2 == 0:
+        result_string += user_symbol1
+    else:
+        result_string += user_symbol2
+
+print(result_string)
 
 """VN: задание не выполнено. Вам надо спросить у пользователя два символа: c1 и c2, а также длину строки.
 Например, пользователь ввёл символы 'S' и 'N', и длину 5. Ваша программа должна вывести 'SNSNS'
 """
+
+#AB: Теперь должно работать =D
+
+
