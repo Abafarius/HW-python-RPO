da42d78a82c4e794655419ca525729b076602b28
Yessimbek Galymzhan <esm.2001@mail.ru>
2023-12-25 19:09:00 +0600

    Update 2.html_title_parser.py

--- 12.12.2023/2.html_title_parser.py
+++ 12.12.2023/2.html_title_parser.py
@@ -8,14 +8,17 @@ except FileNotFoundError:
     exit()
 file.close()
 
-title_start = html_content.find("<p>") + len("<p>")
-title_end = html_content.find("</p>")
-title = html_content[title_start:title_end].strip()
+
 
 headers = ['<h1>', '<h2>', '<h3>', '<h4>', '<h5>', '<h6>']
 for header_tag in headers:
     header_start = html_content.find(header_tag) + len(header_tag)
     header_end = html_content.find('</' + header_tag[1:])
     header_text = html_content[header_start:header_end].strip()
-    print(header_text)
-    print(title)
+    
+    print("Header:", header_text)
+
+title_start = html_content.find("<title>") + len("<title>")
+title_end = html_content.find("</title>")
+title = html_content[title_start:title_end].strip()
+print('title of page: ', title)
