8b22786708e8db132ad32cd89002b8355e2b56a0
gal.esm49 <esm.2001@mail.ru>
2023-12-26 15:09:10 +0600

    Update 3.currency_changer.py

--- 12.12.2023/3.currency_changer.py
+++ 12.12.2023/3.currency_changer.py
@@ -1,13 +1,16 @@
 # Чтение содержимого входного файла
+
 input_file_name = 'currencies.txt'
 with open(input_file_name, 'r', encoding='utf-8') as input_file:
     text = input_file.read()
 
 # Настройка курсов валют (USD и EUR к тенге)
+
 usd_exchange_rate = 430
 eur_exchange_rate = 510
 
 # Поиск и замена сумм в тексте
+
 index = 0
 while True:
     dollar_index = text.find('$', index)
@@ -24,8 +27,10 @@ while True:
         currency = 'EUR'
 
     # Нахождение начала и конца суммы
-    start_index = 0
+
+    start_index = currency_index
     while start_index >= 0 and (text[start_index].isdigit() or text[start_index] == '.' or text[start_index] == ' '):
+        #AS: не могу найти правильное условие где будет находится цифра/число/сумма валюты
         start_index -= 1
     start_index += 1
 
@@ -34,18 +39,22 @@ while True:
         end_index += 1
 
     # Извлечение суммы и конвертация
+  
     amount_str = text[start_index:end_index].strip()
     amount = float(amount_str)
     exchange_rate = usd_exchange_rate if currency == 'USD' else eur_exchange_rate
     converted_amount = amount * exchange_rate
 
     # Замена в тексте
+
     text = text[:start_index] + f'{converted_amount:.2f} ₸' + text[end_index:]
 
     # Обновление индекса для поиска следующей суммы
+
     index = start_index + len(f'{converted_amount:.2f} ₸')
 
 # Запись результата в выходной файл
+
 output_file_name = 'output.txt'
 with open(output_file_name, 'w', encoding='utf-8') as output_file:
     output_file.write(text)
