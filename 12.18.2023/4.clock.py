0625ad93903becec86dfe0f5283cb1f288e452aa
gal.esm49 <esm.2001@mail.ru>
2023-12-27 22:25:03 +0600

    Create 4.clock.py

---
+++ 12.18.2023/4.clock.py
@@ -0,0 +1,26 @@
+def format_time(hours, minutes=None, seconds=None):
+
+    if minutes is None:
+        minutes = 0
+    if seconds is None:
+        seconds = 0
+    
+
+    if not all(isinstance(value, int) for value in [hours, minutes, seconds]):
+        raise ValueError("Все аргументы должны быть целыми числами")
+    
+
+    if not (0 <= hours < 24 and 0 <= minutes < 60 and 0 <= seconds < 60):
+        raise ValueError("Недопустимые значения времени")
+    
+
+    time_string = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+    
+    return time_string
+
+
+hours = 10
+minutes = 30
+seconds = 45
+formatted_time = format_time(hours, minutes, seconds)
+print(formatted_time)
