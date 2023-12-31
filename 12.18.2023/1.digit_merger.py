594a5cdb3a407a1cfbd4963bbcd61d5055fc9a58
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-12-31 23:30:01 +0600

    Проверено дз от 18.12
    Комментарии к коду

--- 12.18.2023/1.digit_merger.py
+++ 12.18.2023/1.digit_merger.py
@@ -1,19 +1,24 @@
 def get_digit_merger(num1,num2,num3):
     if not type(num1) is int and type(num2) is int and type(num3) is int:
         print("argument must be integer")
+        #VN: функция не должна ничего печатать и заниматься проверкой аргументов, если указано, что они num
     else:
         string_num1 = str(num1)
         string_num2 = str(num2)
         string_num3 = str(num3)
         pre_result = string_num1 + string_num2 + string_num3
         print("type of pre_result: ", type(pre_result))
+        #VN: ^^^^^^ делайте функции без побочных эффектов
         try:
             result = int(pre_result)
+            #VN: если все num правильного типа, то исключений, вроде бы, не должно возникнуть
         except ValueError:
             print("Enter integer number!")
             exit()
+            #VN: ^^^^^ делайте функции без побочных эффектов! Функция не должна решать, завершать ли программу!
         print("type of result: ", type(result))
         print("The result is: ", result)
+        #VN:  снова побочные эффекты - функция не должна ничего печатать, если это не её прямое назначение!
     return result
 data1 = input("Enter integer number1: ")
 data2 = input("Enter integer number2: ")
--- 12.18.2023/4.clock.py
+++ 12.18.2023/4.clock.py
@@ -4,6 +4,7 @@ def format_time(hours, minutes=None, seconds=None):
         minutes = 0
     if seconds is None:
         seconds = 0
+    #VN: можно сразу задать им значения по умолчанию 0 и избавиться от этих 4-х строк
     
 
     if not all(isinstance(value, int) for value in [hours, minutes, seconds]):
@@ -12,6 +13,7 @@ def format_time(hours, minutes=None, seconds=None):
 
     if not (0 <= hours < 24 and 0 <= minutes < 60 and 0 <= seconds < 60):
         raise ValueError("Недопустимые значения времени")
+        #VN: ^^^^^^ неплохой вариант с raise!
     
 
     time_string = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
