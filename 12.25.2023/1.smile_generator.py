5442b4d3280d9d53565f1dbb6e2b1cde69e79fe7
gal.esm49 <esm.2001@mail.ru>
2024-01-01 23:50:52 +0600

    Update 1.smile_generator.py

--- 12.25.2023/1.smile_generator.py
+++ 12.25.2023/1.smile_generator.py
@@ -7,10 +7,14 @@ def test_generate_smiley_string():
     assert generate_smiley_string(5) == "😊😊😊😊😊"
     assert generate_smiley_string(0) == ""
     assert generate_smiley_string(1) == "😊"
+    print('done.')
+
+test_generate_smiley_string()
 
 def get_num_smileys_from_user():
+    user_data = input("Enter the count of smiles: ")
     try:
-        num_smileys = int(input("Enter the count of smiles: "))
+        num_smileys = int(user_data)
         return num_smileys
     except ValueError:
         print("Please, enter an integer number.")
