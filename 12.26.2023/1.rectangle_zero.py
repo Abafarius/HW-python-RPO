d37a227b1d38db6e881a0db0dec8635544852b6e
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2024-01-07 23:48:27 +0600

    Проверено дз от 26.12
    Комментарии к коду

--- 12.26.2023/1.rectangle_zero.py
+++ 12.26.2023/1.rectangle_zero.py
@@ -1,24 +1,25 @@
 import random
 
 def create_random_matrix(rows, columns):
-
+#VN: тут была лишняя пустая строка
     matrix = [[random.randint(-20, 20) for _ in range(columns)] for _ in range(rows)]
     return matrix
 
 def print_matrix(matrix):
-
+#VN: тут была лишняя пустая строка
     for row in matrix:
         row_str = " ".join(str(cell) for cell in row)
         print(row_str)
 
 def main():
-
+#VN: тут была лишняя пустая строка
     try:
-
+        #VN: тут была лишняя пустая строка
         rows = int(input("Enter the number of rows: "))
         columns = int(input("Enter the number of columns: "))
 
-
+        #VN: тут была лишняя пустая строка
+        #VN: все последующие строки в блоке try лишние!
         matrix = create_random_matrix(rows, columns)
 
 
