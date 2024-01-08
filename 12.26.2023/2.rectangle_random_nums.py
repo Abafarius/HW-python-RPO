bc5af74393926e680e9975430c552eb6a269c4a7
gal.esm49 <esm.2001@mail.ru>
2024-01-09 01:20:08 +0600

    Update 2.rectangle_random_nums.py

--- 12.26.2023/2.rectangle_random_nums.py
+++ 12.26.2023/2.rectangle_random_nums.py
@@ -1,32 +1,26 @@
 import random
 
 def create_random_matrix(rows, columns):
-
     matrix = [[random.randint(-20, 20) for _ in range(columns)] for _ in range(rows)]
     return matrix
 
 def print_matrix(matrix):
-
     for row in matrix:
         row_str = " ".join(str(cell) for cell in row)
         print(row_str)
 
-def main():
+data1 = input("Enter the number of rows: ")
+data2 =  input("Enter the number of columns: ")
+try:
+    rows = int(data1)
+    columns = int(data2)
+except ValueError:
+    print("Error: Enter integers for the number of rows and columns.")
+    exit()
 
-    try:
-
-        rows = int(input("Введите количество строк: "))
-        columns = int(input("Введите количество столбцов: "))
-
-
-        matrix = create_random_matrix(rows, columns)
+matrix = create_random_matrix(rows, columns)
+print("Your matrix:")
+print_matrix(matrix)
 
 
-        print("Ваша матрица:")
-        print_matrix(matrix)
 
-    except ValueError:
-        print("Error: Enter integers for the number of rows and columns.")
-
-if __name__ == "__main__":
-    main()
