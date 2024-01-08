c4f61280efbab492ddac104d300fc03717be1c20
gal.esm49 <esm.2001@mail.ru>
2024-01-09 02:19:53 +0600

    Update 2.rectangle_area.py

--- 12.18.2023/2.rectangle_area.py
+++ 12.18.2023/2.rectangle_area.py
@@ -1,15 +1,9 @@
 def get_rectanlge_area(length, width = None):
-    if type(length) is int or type(width) is int:
-        if not width is None:
-            area = length*width
-            print("The area of rectangle: ", area)
-            return area
-        square_area = length**2
-        print("The area of square: ", square_area)
-        return square_area
-    else:
-        print("Args must be integer numbers!")
-
+    if not width is None:
+        area = length*width
+        return area
+    square_area = length**2
+    return square_area
 
 data1 = input("Enter an integer number for length: ")
 try:
@@ -19,18 +13,18 @@ except ValueError:
     exit()
 
 data2 = input("Enter an integer number for width: ")
-try:
-    value2 = int(data2)
-except ValueError:
-    print("It's not an integer number!")
-    exit()
+if data2 == "":
+    value2 = None
+else:
+    try:
+        value2 = int(data2)
+    except ValueError:
+        print("It's not an integer number!")
+        exit()
 
-
-try:
-    get_rectanlge_area(value1, value2)
-except TypeError:
-    print("Function must have just 2 args")
-    exit()
-except NameError:
-    print("Args have to be an integer numbers")
-    exit()
+if value2 == None:
+    square_area =  get_rectanlge_area(value1, value2)
+    print("The area of square: ", square_area)
+else:
+    area = get_rectanlge_area(value1, value2)
+    print("The area of rectangle: ", area)
