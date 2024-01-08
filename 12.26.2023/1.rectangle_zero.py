159c5925ee4b61ca25d46617097935cdd5177521
gal.esm49 <esm.2001@mail.ru>
2024-01-09 01:20:06 +0600

    Update 1.rectangle_zero.py

--- 12.26.2023/1.rectangle_zero.py
+++ 12.26.2023/1.rectangle_zero.py
@@ -1,33 +1,24 @@
-import random
-
-def create_random_matrix(rows, columns):
-#VN: тут была лишняя пустая строка
-    matrix = [[random.randint(-20, 20) for _ in range(columns)] for _ in range(rows)]
+def create_matrix(rows, columns):
+    matrix = [[0] * columns for _ in range(rows)]
     return matrix
 
 def print_matrix(matrix):
-#VN: тут была лишняя пустая строка
     for row in matrix:
         row_str = " ".join(str(cell) for cell in row)
         print(row_str)
 
-def main():
-#VN: тут была лишняя пустая строка
-    try:
-        #VN: тут была лишняя пустая строка
-        rows = int(input("Enter the number of rows: "))
-        columns = int(input("Enter the number of columns: "))
+data1 = input("Enter the number of rows: ")
+data2 =  input("Enter the number of columns: ")
 
-        #VN: тут была лишняя пустая строка
-        #VN: все последующие строки в блоке try лишние!
-        matrix = create_random_matrix(rows, columns)
+try:
+    rows = int(data1)
+    columns = int(data2)
+except ValueError:
+    print("Error: Enter integers for the number of rows and columns.")
+    exit()
+
+matrix = create_matrix(rows, columns)
+print("Your matrix:")
+print_matrix(matrix)
 
 
-        print("Your matrix:")
-        print_matrix(matrix)
-
-    except ValueError:
-        print("Error: Enter integers for the number of rows and columns.")
-
-if __name__ == "__main__":
-    main()
