43dc4e015310b78f20e03135ff30673693ea52aa
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-01-09 21:43:46 +0600

    Create map.py

---
+++ CW_py_(01.09.2024)/map.py
@@ -0,0 +1,18 @@
+array = ['Timur', 'Argyn', 'Sergey', 'Almat', 'Slava', 'Alexander', 'Galym', 'Vitaliy']
+new_array = sorted(array, key = lambda a,: len(a))
+print(new_array)
+
+#Predicate  это функция-предикат или функция-вопрос. Предикат отвечает на утвердительный вопрос «да» или «нет», возвращая значение типа bool.
+
+#1. map
+#2. filter
+#3. reduce
+
+result = list(map(lambda x: x[-1], array))
+print(result)
+
+user_list = ["2", "4", "45"]
+numbers = list(map(int, user_list))
+print(numbers)
+
+
