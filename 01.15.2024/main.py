9c69bbce7dd77b3694e9d836115036b8f59696f3
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-01-16 21:43:07 +0600

    Update main.py

--- 01.15.2024/main.py
+++ 01.15.2024/main.py
@@ -1,11 +1,11 @@
-import pygame as pg
-pg.init()
-screen = pg.display.set_mode((800, 600))
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
 running = True
 while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
     screen.fill((0, 0, 0))
-    pg.display.update()
-pg.quit()
+    pygame.display.update()
+pygame.quit()
