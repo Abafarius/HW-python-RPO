f0bed9a73163f903243f97542f6fabba917ff56e
gal.esm49 <esm.2001@mail.ru>
2024-01-26 21:07:06 +0600

    Update mammals.py

--- 01.22.2024/Classes/mammals.py
+++ 01.22.2024/Classes/mammals.py
@@ -1,21 +1,45 @@
 class Mammals:
     gender = 0
     weight = 0
-    size = 0
+    height = 0
     age = 0
     type_food_eater = ""
+    health = 100
 
 
-    def __init__(self, age):
+    def __init__(self, age, type_food_eater, gender):
         self.age = age
-        return self.age
-    
-    def get_type_food_eater(self, food_eater):
-        self.type_food_eater = food_eater
-        return Mammals()
+        self.type_food_eater = type_food_eater
+        self.gender = gender
+        self.weight += age*1.8
+        self.height += self.weight + 100
+
+    def get_grow(self, age):
+        self.weight += 5
+        self.height += 100+self.weight
+        self.age = age 
+
+    def get_feed(self, food_num):
+        self.weight += food_num/100
+
+    def get_attack(self, other_self, damage):
+        if self.weight > other_self.weight:
+            self.health -= damage*0.2
+            other_self.health -= damage
+        else:
+            self.health -= damage
+            other_self.health -= damage*0.2            
 
 
-cat = Mammals(2)
-predator = cat.get_type_food_eater("predator")
+cat = Mammals(2, "predator", "female")
+print(cat.age)
+cat.get_grow(10)
+print(cat.age, cat.weight, cat.height, cat.gender, cat.type_food_eater)
+
+cat.get_feed(306)
+print(cat.weight)
+
+whale = Mammals(150, "everything", "female")
+whale.get_attack(cat, 100)
     
-        
+print(whale.health, cat.health)        
