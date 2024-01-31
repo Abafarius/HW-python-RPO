46ad937b1965627ec6997b0b77096032bd88e743
gal.esm49 <esm.2001@mail.ru>
2024-01-31 17:53:20 +0600

    Update books.py

--- 01.22.2024/Classes/books.py
+++ 01.22.2024/Classes/books.py
@@ -25,7 +25,34 @@ class Books:
         self.pages += page
 
     def get_older(self, years):
-        self.age = years
-        self.health
+        self.age += years
+        if self.age < 10:
+            self.health -= self.age * 0.01
+        elif self.age < 100:
+            self.health -= self.age * 0.9
+        elif self.age < 1000:
+            self.health -= self.age *2.5
 
-        
+    def get_tear_book(self, page, damage):
+        self.health -= damage
+        self.pages -= page    
+
+war_of_the_worlds = Books(50, 50, 288, 10, "sci-fi")
+print("Информация о книге:")
+print("Название:", "War of the Worlds")
+print("Ширина:", war_of_the_worlds.width)
+print("Высота:", war_of_the_worlds.height)
+print("Количество страниц:", war_of_the_worlds.pages)
+print("Жанр:", war_of_the_worlds.genre)
+print("Здоровье:", war_of_the_worlds.health)
+print("Возраст:", war_of_the_worlds.age)
+
+war_of_the_worlds.get_new_page(10)
+print("После чтения 10 страниц:", "Количество страниц:", war_of_the_worlds.pages)
+
+war_of_the_worlds.get_older(5)
+print("После старения на 5 лет:", "Здоровье:", war_of_the_worlds.health)
+
+war_of_the_worlds.get_tear_book(50, 400)
+print("После повреждения книги:", "Здоровье:", war_of_the_worlds.health)
+print("После повреждения книги:", "Количество страниц:", war_of_the_worlds.pages)
