e4996e1325962aba13763677916ac95b6aa52a41
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-02-22 21:48:02 +0600

    Create netsender.py

---
+++ CW_py_(02.22.2024)/netsender.py
@@ -0,0 +1,13 @@
+import socket
+class Sender:
+    port = 11488
+    sock = None
+
+    def __init__(self, port):
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send(self, message, dest_ip):
+        data = message.encode('utf8')
+        addr = (dest_ip, self.port)
+        self.sock.sendto(data, addr)
